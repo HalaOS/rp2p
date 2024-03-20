32b36189de665441a71d3aae714d628a2bb79da0
yayanyang <yayanyang@gmail.com>
2024-03-20 21:46:56 +0800

    update

--- crates/libp2p/src/channel/tls.rs
+++ crates/libp2p/src/channel/tls.rs
@@ -6,9 +6,9 @@ use std::{
 };
 
 use boring::ssl::{MidHandshakeSslStream, SslAcceptor, SslConnector, SslMethod, SslStream};
-use bytes::BytesMut;
+use bytes::{Buf, BytesMut};
 use multiaddr::Multiaddr;
-use rasi::syscall::{CancelablePoll, Handle};
+use rasi::syscall::{CancelablePoll, Handle, PendingHandle};
 use rasi_ext::utils::{Lockable, LockableNew, SpinMutex};
 
 use crate::{errors::P2pError, ChannelIo, HandleContext, SecureUpgrade, Transport};
@@ -20,9 +20,10 @@ struct TlsBuffer {
     handle: Arc<Handle>,
     transport: std::sync::Arc<Box<dyn Transport>>,
     send_buf: Option<BytesMut>,
-    recv_buf: Option<BytesMut>,
     read_waker: Option<Waker>,
     write_waker: Option<Waker>,
+    read_pending: Option<PendingHandle>,
+    write_pending: Option<PendingHandle>,
 }
 
 impl TlsBuffer {
@@ -31,9 +32,10 @@ impl TlsBuffer {
             handle,
             transport,
             send_buf: None,
-            recv_buf: None,
             read_waker: None,
             write_waker: None,
+            read_pending: None,
+            write_pending: None,
         }
     }
     pub fn register_read(&mut self, waker: Waker) {
@@ -42,6 +44,14 @@ impl TlsBuffer {
 
     pub fn register_write(&mut self, waker: Waker) {
         self.write_waker = Some(waker);
+    }
+
+    pub fn is_read_pending(&self) -> bool {
+        self.read_waker.is_some()
+    }
+
+    pub fn is_write_pending(&self) -> bool {
+        self.write_waker.is_some()
     }
 }
 
@@ -59,7 +69,20 @@ impl io::Write for TlsBuffer {
 #[allow(unused)]
 impl io::Read for TlsBuffer {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        todo!()
+        let read_waker = self.read_waker.take().unwrap();
+        match self.transport.read(
+            &mut Context::from_waker(&read_waker),
+            &self.handle,
+            buf,
+            self.read_pending.take(),
+        ) {
+            CancelablePoll::Ready(r) => return r,
+            CancelablePoll::Pending(read_pending) => {
+                self.read_pending = read_pending;
+                self.read_waker = Some(read_waker);
+                return Ok(0);
+            }
+        }
     }
 }
 enum TlsState {
@@ -86,31 +109,55 @@ impl TlsStream {
         }
     }
 
-    pub fn write(&self, cx: &mut Context<'_>, buf: &[u8]) -> io::Result<usize> {
+    pub fn write(&self, cx: &mut Context<'_>, buf: &[u8]) -> CancelablePoll<io::Result<usize>> {
         let mut state = self.state.lock();
 
         match &mut *state {
             TlsState::Stream(stream) => {
                 stream.get_mut().register_write(cx.waker().clone());
-                stream.write(buf)
+                match stream.write(buf) {
+                    Ok(write_size) => {
+                        if write_size == 0 && stream.get_mut().is_write_pending() {
+                            return CancelablePoll::Pending(None);
+                        }
+
+                        return CancelablePoll::Ready(Ok(write_size));
+                    }
+                    Err(err) => {
+                        return CancelablePoll::Ready(Err(err));
+                    }
+                }
             }
-            TlsState::Handshake(_) => {
-                Err(io::Error::new(io::ErrorKind::Other, "Call handshake first"))
-            }
+            TlsState::Handshake(_) => CancelablePoll::Ready(Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Call handshake first",
+            ))),
         }
     }
 
-    pub fn read(&self, cx: &mut Context<'_>, buf: &mut [u8]) -> io::Result<usize> {
+    pub fn read(&self, cx: &mut Context<'_>, buf: &mut [u8]) -> CancelablePoll<io::Result<usize>> {
         let mut state = self.state.lock();
 
         match &mut *state {
             TlsState::Stream(stream) => {
                 stream.get_mut().register_read(cx.waker().clone());
-                stream.read(buf)
+                match stream.read(buf) {
+                    Ok(write_size) => {
+                        if write_size == 0 && stream.get_mut().is_read_pending() {
+                            return CancelablePoll::Pending(None);
+                        }
+
+                        return CancelablePoll::Ready(Ok(write_size));
+                    }
+                    Err(err) => {
+                        return CancelablePoll::Ready(Err(err));
+                    }
+                }
             }
-            TlsState::Handshake(_) => {
-                Err(io::Error::new(io::ErrorKind::Other, "Call handshake first"))
-            }
+            TlsState::Handshake(_) => CancelablePoll::Ready(Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Call handshake first",
+            ))),
         }
     }
 
@@ -169,19 +216,7 @@ impl ChannelIo for TlsSecureUpgrade {
 
         let stream = handle.downcast::<TlsStream>().expect("Expect TlsStream");
 
-        match stream.write(cx, buf) {
-            Ok(write_size) => {
-                // the buf is full.
-                if write_size == 0 {
-                    return CancelablePoll::Pending(None);
-                }
-
-                return CancelablePoll::Ready(Ok(write_size));
-            }
-            Err(err) => {
-                return CancelablePoll::Ready(Err(err));
-            }
-        }
+        stream.write(cx, buf)
     }
 
     fn read(
@@ -197,19 +232,7 @@ impl ChannelIo for TlsSecureUpgrade {
 
         let stream = handle.downcast::<TlsStream>().expect("Expect TlsStream");
 
-        match stream.read(cx, buf) {
-            Ok(read_size) => {
-                // the buf is empty.
-                if read_size == 0 {
-                    return CancelablePoll::Pending(None);
-                }
-
-                return CancelablePoll::Ready(Ok(read_size));
-            }
-            Err(err) => {
-                return CancelablePoll::Ready(Err(err));
-            }
-        }
+        stream.read(cx, buf)
     }
 
     fn shutdown(&self, handle: &Handle, how: std::net::Shutdown) -> io::Result<()> {
